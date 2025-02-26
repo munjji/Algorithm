17a375d0588ee0af30b1388329790e5da35ce659
Jihee Lee <129031670+munjji@users.noreply.github.com>
2025-02-26 22:45:15 +0900

    [level 2] Title: 예상 대진표, Time: 0.28 ms, Memory: 91.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+                
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            answer++;
+        }
+        
+        System.out.println(answer);
+
+        return answer;
+    }
+}
