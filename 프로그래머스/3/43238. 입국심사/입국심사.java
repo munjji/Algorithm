a1eb91869f5fa7dd7528879e8192f752f46acf33
Jihee Lee <129031670+munjji@users.noreply.github.com>
2025-03-11 18:15:24 +0900

    [level 3] Title: 입국심사, Time: 110.91 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        Arrays.sort(times);
+        long min = 1;
+        long max = (long)times[times.length-1] * n;
+        
+        while (min <= max) {
+            // 현재 탐색 중인 시간
+            long mid = (min + max) / 2;
+            // 탐색 인원
+            long person = 0;
+            
+            for (int time: times) {
+                person += mid / time;
+                
+                if (person >= n) {
+                    break;
+                }
+            }
+            
+            if (person >= n) {
+                answer = mid;
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        
+        return answer;        
+    }
+}
