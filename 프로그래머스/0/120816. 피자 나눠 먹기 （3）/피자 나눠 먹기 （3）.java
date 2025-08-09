484b926f5a6b80b092f850408cded65609ade1c8
이지희 <129031670+munjji@users.noreply.github.com>
2025-08-09 22:56:40 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        if (n % slice == 0) {
+            return n / slice;
+        }
+        answer = n / slice + 1;
+        return answer;
+    }
+}
