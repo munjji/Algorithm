c4c36a7244bdf36dd4a7cb3dadb8e0033da06c72
이지희 <129031670+munjji@users.noreply.github.com>
2025-08-10 12:51:42 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.04 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        answer = new StringBuilder(my_string).reverse().toString();
+        return answer;
+    }
+}
