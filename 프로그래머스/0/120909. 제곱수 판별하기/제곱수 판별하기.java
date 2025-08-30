e2cc986d2ab33aa45b1f87a6d8bd0e270c49c092
이지희 <129031670+munjji@users.noreply.github.com>
2025-08-30 22:21:39 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int sqrt = (int) Math.sqrt(n);
+        if (sqrt * sqrt == n) return 1;
+        else return 2;
+    }
+}
