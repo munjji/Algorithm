1103cb5e7acc33ac9e0708bd013997d45bdaae1f
이지희 <129031670+munjji@users.noreply.github.com>
2025-09-18 14:13:53 +0900

    [level 0] Title: 문자열 밀기, Time: 16.04 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String A, String B) {
+        String rotated = A;        
+        
+        if (A.equals(B)) return 0;
+        
+        for (int i = 1; i <= A.length(); i++) {
+            rotated = rotated.charAt(rotated.length() - 1) + rotated.substring(0, rotated.length() - 1);
+            
+            if (rotated.equals(B)) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
