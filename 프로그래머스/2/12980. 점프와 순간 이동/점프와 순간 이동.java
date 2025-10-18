9e0d97d30c9241f393cde04b477d7ba288eed0be
이지희 <129031670+munjji@users.noreply.github.com>
2025-10-18 09:56:29 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        
+        int move = 1;
+        
+        while (n > 0) {
+            if (n % 2 != 0) {
+                n -= 1;
+                ans++;
+            }
+            else {
+                n /= 2;
+            }
+        }
+
+        return ans;
+    }
+}
