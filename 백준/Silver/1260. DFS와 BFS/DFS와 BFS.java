625f85847050a7c30f6a404ca7c841e9d8dd9e7e
이지희 <129031670+munjji@users.noreply.github.com>
2025-10-29 19:02:38 +0900

    [Silver II] Title: DFS와 BFS, Time: 288 ms, Memory: 21800 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        visited = new boolean[N + 1];
+        dfs(V);
+        System.out.println();
+
+        visited = new boolean[N + 1];
+        bfs(V);
+    }
+
+    public static void dfs(int x) {
+        visited[x] = true;
+        System.out.print(x + " ");
+
+        for (int i = 0; i < graph.get(x).size(); i++) {
+            int y = graph.get(x).get(i);
+            if (!visited[y]) dfs(y);
+        }
+    }
+
+    public static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            System.out.print(x + " ");
+
+            for (int i = 0; i < graph.get(x).size(); i++) {
+                int y = graph.get(x).get(i);
+                if (!visited[y]) {
+                    q.offer(y);
+                    visited[y] = true;
+                }
+            }
+        }
+    }
+}
