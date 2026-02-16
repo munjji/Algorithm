9f98b137eed22e9ca927d7fd04cd320efe42083e
이지희 <129031670+munjji@users.noreply.github.com>
2026-02-16 15:31:14 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 104 MB, 시간: 0.25 ms
+메모리: 95.1 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 01일 13:50:07
+2026년 02월 16일 15:31:11
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,12 +1,8 @@
-// Stack, Queue => 앞에서 부터 빠져야 하니까 Queue 사용
-// day = 100 - progresses[i] / speeds[i];
+// day = (100 - progresses[i]) / speeds[i];
 // progresses = [7, 3, 9]으로 재정의 하기
 // 처음 거랑 그 다음 거랑 비교해서 만약에 그 다음 게 작으면 같이 remove 하자
 // 개수 세서 speeds에 추가하자
 
-// 반례입니다.
-// 입력값 〉 [90, 90], [10, 9] => [1, 2]
-// 기댓값 〉 [1, 1]
 import java.util.*;
 
 class Solution {
@@ -21,20 +17,20 @@ class Solution {
             proQueue.offer(day);
         }
         
-        int first = proQueue.poll();
+        int cur = proQueue.poll();
         int count = 1;
         
         // 개수 세기
         while (!proQueue.isEmpty()) {
             int next = proQueue.peek();
             
-            if (first >= next) {
+            if (cur >= next) {
                 proQueue.poll();
                 count++;
             } else {
                 answerList.add(count);
                 count = 1;
-                first = proQueue.poll();
+                cur = proQueue.poll();
             }
         }
         
