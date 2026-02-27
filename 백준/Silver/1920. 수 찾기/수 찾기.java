23a819ca763ea0f04ec0f67c0253913f6d143764
이지희 <129031670+munjji@users.noreply.github.com>
2026-02-27 14:36:01 +0900

    [Silver IV] Title: 수 찾기, Time: 1088 ms, Memory: 52104 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            int answer = binarySearch(arr, target);
+            System.out.println(answer);
+        }
+    }
+
+    static int binarySearch (int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) return 1;
+            else if (arr[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return 0;
+    }
+}
