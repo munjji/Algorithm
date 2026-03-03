c027820484c15c3267f27b633338835abaad18ec
이지희 <129031670+munjji@users.noreply.github.com>
2026-03-03 15:52:19 +0900

    [Gold IV] Title: 최단경로, Time: 864 ms, Memory: 111208 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static private class Node implements Comparable<Node> {
+        int to;
+        int cost;
+
+        Node(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    static int V, E;
+    static List<Node>[] graph;
+    static int[] dist;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        int start = Integer.parseInt(br.readLine());
+
+        graph = new ArrayList[V + 1];
+        for (int i = 1; i <= V; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph[from].add(new Node(to, cost));
+        }
+
+        dijkstra(start);
+
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] == Integer.MAX_VALUE) System.out.println("INF");
+            else System.out.println(dist[i]);
+        }
+    }
+
+    static void dijkstra(int start) {
+        dist = new int[V + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        dist[start] = 0;
+        pq.add(new Node(start, 0));
+
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+
+            if (dist[cur.to] < cur.cost) continue;
+
+            for (Node next : graph[cur.to]) {
+                if (dist[next.to] > dist[cur.to] + next.cost) {
+                    dist[next.to] = dist[cur.to] + next.cost;
+                    pq.add(new Node(next.to, dist[next.to]));
+                }
+            }
+        }
+    }
+}
