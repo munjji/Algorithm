23c1b43fa5854031dbd2d6ae5c51772589574982
이지희 <129031670+munjji@users.noreply.github.com>
2026-03-06 15:17:20 +0900

    [Silver I] Title: 영역 구하기, Time: 132 ms, Memory: 14944 KB -BaekjoonHub

---
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int M, N, K;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        // 왼쪽 아래, 오른쪽 위
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            for (int x = x1; x < x2; x++) {
+                for (int y = y1; y < y2; y++) {
+                    map[x][y] = 1;
+                }
+            }
+        }
+
+        int cnt = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0 && !visited[i][j]) {
+                    list.add(bfs(i, j));
+                    cnt++;
+                }
+            }
+        }
+
+        Collections.sort(list);
+        System.out.println(cnt);
+        for (int width : list) {
+            System.out.print(width + " ");
+        }
+
+    }
+
+    static int bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+        visited[x][y] = true;
+        int width = 1;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            for (int d = 0; d < 4; d++) {
+                int nx = cx + dx[d];
+                int ny = cy + dy[d];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (visited[nx][ny]) continue;
+                if (map[nx][ny] == 1) continue;
+
+                visited[nx][ny] = true;
+                width++;
+                q.add(new int[]{nx, ny});
+            }
+        }
+
+        return width;
+    }
+}
