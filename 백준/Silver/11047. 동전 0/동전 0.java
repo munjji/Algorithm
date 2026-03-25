9240bb60b8c3c6962a6a0e18ed683bdb5afab27e
이지희 <129031670+munjji@users.noreply.github.com>
2026-03-25 17:39:39 +0900

    [Silver IV] Title: 동전 0, Time: 108 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer  st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            coins[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (coins[i] <= K) {
+                answer += (K / coins[i]);
+                K = K % coins[i];
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
