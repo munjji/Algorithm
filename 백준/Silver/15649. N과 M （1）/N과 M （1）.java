c76b2222a54e8daca543dccf474dcad068f04b70
이지희 <129031670+munjji@users.noreply.github.com>
2026-03-25 18:05:10 +0900

    [Silver III] Title: N과 M (1), Time: 1904 ms, Memory: 67956 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int[] nums;
+    static int[] result;
+    static boolean[] visited;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer  st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+        visited = new boolean[N];
+        result = new int[N];
+
+        for (int i = 1; i <= N; i++) {
+            nums[i - 1] = i;
+        }
+
+        dfs(0);
+    }
+
+    static private void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) continue;
+            
+            visited[i] = true;
+            result[depth] = nums[i];
+            dfs(depth + 1);
+            visited[i] = false;
+        }
+    }
+}
