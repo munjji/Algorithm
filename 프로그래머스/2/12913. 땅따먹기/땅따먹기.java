383311885e2b4dda6ae8111af542a194fc777422
이지희 <129031670+munjji@users.noreply.github.com>
2026-04-16 16:46:46 +0900

    [level 2] Title: 땅따먹기, Time: 29.32 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    int solution(int[][] land) {
+        int n = land.length;
+        int[][] dp = new int[n][4];
+
+        for (int i = 0; i < 4; i++) {
+            dp[0][i] = land[0][i];
+        }
+        
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = land[i][0] + Math.max(Math.max(dp[i - 1][1], dp[i - 1][2]), dp[i - 1][3]);
+            dp[i][1] = land[i][1] + Math.max(Math.max(dp[i - 1][0], dp[i - 1][2]), dp[i - 1][3]);
+            dp[i][2] = land[i][2] + Math.max(Math.max(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][3]);
+            dp[i][3] = land[i][3] + Math.max(Math.max(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]);
+        }
+        
+        return Math.max(Math.max(dp[n - 1][0], dp[n - 1][1]),
+                        Math.max(dp[n - 1][2], dp[n - 1][3]));
+    }
+}
