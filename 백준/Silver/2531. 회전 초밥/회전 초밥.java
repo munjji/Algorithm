a7425874fcb7f27808ecef04c5089deeeb05e76d
이지희 <129031670+munjji@users.noreply.github.com>
2026-04-23 12:18:37 +0900

    [Silver I] Title: 회전 초밥, Time: 164 ms, Memory: 18392 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -5,41 +5,43 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer  st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 회전 초밥 접시의 수
+        int N = Integer.parseInt(st.nextToken()); // 총 접시 수
         int d = Integer.parseInt(st.nextToken()); // 초밥 가짓수
         int k = Integer.parseInt(st.nextToken()); // 연속해서 먹는 접시 수
         int c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
 
         int[] sushi = new int[N];
+
         for (int i = 0; i < N; i++) {
             sushi[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] cnt = new int[d + 1];
-        int kind = 0;
+        int[] count = new int[d + 1];
+        int unique = 0;
+        int max = 0;
 
         for (int i = 0; i < k; i++) {
-            if (cnt[sushi[i]] == 0) kind++;
-            cnt[sushi[i]]++;
+            if (count[sushi[i]] == 0) unique++;
+            count[sushi[i]]++;
         }
 
-        int answer = kind + (cnt[c] == 0 ? 1 : 0);
+        max = unique + (count[c] == 0 ? 1 : 0);
 
-        for (int i = 1; i < N; i++) {
-            int remove = sushi[i - 1];
-            cnt[remove]--;
-            if (cnt[remove] == 0) kind--;
+        for (int left = 1; left < N; left++) {
+            int right = (left + k - 1) % N;
 
-            int add = sushi[(i + k - 1) % N];
-            if (cnt[add] == 0) kind++;
-            cnt[add]++;
+            count[sushi[left - 1]]--;
+            if (count[sushi[left - 1]] == 0) unique--;
 
-            int current = kind + (cnt[c] == 0 ? 1 : 0);
-            answer = Math.max(answer, current);
+            if (count[sushi[right]] == 0) unique++;
+            count[sushi[right]]++;
+
+            int sum = unique + (count[c] == 0 ? 1 : 0);
+            max = Math.max(max, sum);
         }
 
-        System.out.println(answer);
+        System.out.print(max);
     }
 }
