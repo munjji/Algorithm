e0c31ab8ec441dfec21e2228cf77439b19b03e37
이지희 <129031670+munjji@users.noreply.github.com>
2026-04-26 12:10:57 +0900

    [Gold IV] Title: 오큰수, Time: 964 ms, Memory: 176252 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -7,32 +7,33 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        Stack<Integer> stack = new Stack<>(); // 인덱스를 넣기
+        int N = Integer.parseInt(br.readLine()); // N
 
+        int[] nums = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
+        Stack<Integer> stack = new Stack<>(); // 인덱스 넣기
         int[] result = new int[N];
+
         for (int i = 0; i < N; i++) {
-            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
-                result[stack.pop()] = arr[i];
+            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
+                result[stack.pop()] = nums[i];
             }
             stack.push(i);
         }
-
+        
         while (!stack.isEmpty()) {
             result[stack.pop()] = -1;
         }
-
+        
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(result[i]).append(" ");
+        for (int r : result) {
+            sb.append(r + " ");
         }
-
-        System.out.print(sb.toString());
+        
+        System.out.print(sb);
     }
 }
