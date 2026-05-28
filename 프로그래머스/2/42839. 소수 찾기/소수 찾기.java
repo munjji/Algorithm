b3a81228d010037eb13ee9214b76efea7a7ff208
이지희 <129031670+munjji@users.noreply.github.com>
2026-05-28 13:22:12 +0900

    [level 2] Title: 소수 찾기, Time: 14.57 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,33 +1,36 @@
 import java.util.*;
 
 class Solution {
-    static Set<Integer> primes = new HashSet<>();
+    
+    static HashSet<Integer> primes = new HashSet<>();
+    static char[] numArray;
+    static boolean[] visited;
     
     public int solution(String numbers) {
-        char[] numArray = numbers.toCharArray();
-        boolean[] used = new boolean[numArray.length];
+        numArray = numbers.toCharArray();
+        visited = new boolean[numArray.length];
 
-        generateNumbers(numArray, "", used);
+        generateNumbers("");
         
         return countPrimes(primes);
     }
     
-    // 모든 숫자 조합 생성 (백트래킹)
-    private void generateNumbers(char[] numArray, String current, boolean[] used) {
-        if (!current.isEmpty()) {
-            primes.add(Integer.parseInt(current)); // 숫자로 변환하여 Set에 추가
+    // 모든 숫자 조합 생성 (백트래킹 함수)
+    private void generateNumbers(String current) {
+        if (!current.equals("")) {
+            primes.add(Integer.parseInt(current));
         }
 
         for (int i = 0; i < numArray.length; i++) {
-            if (!used[i]) {
-                used[i] = true;
-                generateNumbers(numArray, current + numArray[i], used);
-                used[i] = false;
-            }
+            if (visited[i]) continue;
+            
+            visited[i] = true;
+            generateNumbers(current + numArray[i]);
+            visited[i] = false;
         }
     }
     
-    // 소수 판별
+    // 소수 개수
     private int countPrimes(Set<Integer> numbers) {
         int count = 0;
         for (int num : numbers) {
@@ -36,7 +39,7 @@ class Solution {
         return count;
     }
 
-    // 소수 판별 함수
+    // 소수 판별
     private boolean isPrime(int num) {
         if (num < 2) return false;
         for (int i = 2; i <= Math.sqrt(num); i++) {
