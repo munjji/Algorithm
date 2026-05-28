a513d570c8b95b7dd28fb3318658175a3aad1d99
이지희 <129031670+munjji@users.noreply.github.com>
2026-05-28 12:46:51 +0900

    [level 1] Title: 모의고사, Time: 2.04 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = {};
         int[] no1 = {1, 2, 3, 4, 5};
         int[] no2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] no3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
@@ -18,13 +17,15 @@ class Solution {
         int[] cntArray = {no1Cnt, no2Cnt, no3Cnt};
         int maxScore = Math.max(no1Cnt, Math.max(no2Cnt, no3Cnt));
         
-        List<Integer> result = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             if (cntArray[i] == maxScore) {
-                result.add(i + 1);
+                answer.add(i + 1);
             }
         }
         
-        return result.stream().mapToInt(i -> i).toArray();
+        return answer.stream()
+            .mapToInt(i -> i)
+            .toArray();
     }
 }
