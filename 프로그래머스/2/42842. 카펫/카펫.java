8d42c3b86e6ebca52e7d52dbf990bbac2faab3c7
이지희 <129031670+munjji@users.noreply.github.com>
2026-05-28 13:28:39 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -4,17 +4,18 @@ class Solution {
         int n = brown + yellow;
         int h = 0;
         
-        for (int i = brown / 2; i > 0 ; i--) {
-            if (n % i == 0) {
-                h = n / i;
+        for (int w = brown / 2; w > 0 ; w--) {
+            if (n % w == 0) {
+                h = n / w;
             }
             
-            if (i * 2 + 2 * h - 4 == brown && (i -2) * (h-2) == yellow) {
-                answer[0] = i;
+            if ((w * 2) + (h * 2) - 4 == brown && (w - 2) * (h - 2) == yellow) {
+                answer[0] = w;
                 answer[1] = h;
                 break;
             }
         }
+        
         return answer;
     }
 }
