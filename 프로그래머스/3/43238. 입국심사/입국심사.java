0a6dbf2cd0053f867e2c89520cb749e3fded6d05
이지희 <129031670+munjji@users.noreply.github.com>
2026-05-29 11:25:18 +0900

    [level 3] Title: 입국심사, Time: 50.17 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,17 +3,16 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
+        int m = times.length;
         Arrays.sort(times);
+        
         long min = 1;
-        long max = (long)times[times.length-1] * n;
+        long max = (long) n * times[m - 1];
         
         while (min <= max) {
-            // 현재 탐색 중인 시간
             long mid = (min + max) / 2;
-            // 탐색 인원
             long person = 0;
             
-            // 시간 안에 모든 심사관이 n명을 처리할 수 있는지 확인
             for (int time: times) {
                 person += mid / time;
                 
@@ -22,10 +21,9 @@ class Solution {
                 }
             }
             
-            // mid 값 조정
             if (person >= n) {
-                answer = mid;
                 max = mid - 1;
+                answer = mid;
             } else {
                 min = mid + 1;
             }
