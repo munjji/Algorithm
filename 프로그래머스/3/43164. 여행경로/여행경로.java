afe3656b1925eb0af371f4a739a8971957457c1c
이지희 <129031670+munjji@users.noreply.github.com>
2026-05-29 14:20:28 +0900

    [level 3] Title: 여행경로, Time: 0.42 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 class Solution {
-    List<String> path = new ArrayList<>();
-    boolean[] used;
+    
+    static List<String> path = new ArrayList<>();
+    static boolean[] used;
 
     public String[] solution(String[][] tickets) {
-        // 항공권 수만큼의 방문 여부 배열
         used = new boolean[tickets.length];
 
         // 사전 순 정렬
@@ -20,24 +20,25 @@ class Solution {
         // 리스트를 배열로 변환해 반환
         return path.toArray(new String[0]);
     }
-
-    private boolean dfs(String current, String[][] tickets, int count) {
-        // 모든 티켓 사용 완료
-        if (count == tickets.length) return true;
-
+    
+    static private boolean dfs(String current, String[][] tickets, int cnt) {
+        if (cnt == tickets.length) {
+            return true;
+        }
+        
         for (int i = 0; i < tickets.length; i++) {
-            if (!used[i] && tickets[i][0].equals(current)) {
+            if (used[i]) continue;
+            if (tickets[i][0].equals(current)) {
                 used[i] = true;
                 path.add(tickets[i][1]);
-
-                if (dfs(tickets[i][1], tickets, count + 1)) return true;
-
-                // 백트래킹
+                
+                if (dfs(tickets[i][1], tickets, cnt + 1)) return true;
+                
                 used[i] = false;
                 path.remove(path.size() - 1);
             }
         }
-
+        
         return false;
     }
 }
