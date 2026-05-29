58f4bc2a3798d6058402c5dcb84b045b15e345a4
이지희 <129031670+munjji@users.noreply.github.com>
2026-05-29 13:34:33 +0900

    [level 2] Title: 타겟 넘버, Time: 4.65 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,16 +1,25 @@
+import java.util.*;
+
 class Solution {
+    static int[] numbers;
+    static int target;
+        
     public int solution(int[] numbers, int target) {
-        return dfs(numbers, 0, 0, target);
+        this.numbers = numbers;
+        this.target = target;
+        
+        return dfs(0, 0);
     }
 
-    private int dfs(int[] numbers, int idx, int sum, int target) {
+    private int dfs(int idx, int sum) {
         if (idx == numbers.length) {
             return sum == target ? 1 : 0;
         }
         
         // 현재 숫자에 +, -를 붙여 다음으로 진행
-        int withPlus  = dfs(numbers, idx + 1, sum + numbers[idx], target);
-        int withMinus = dfs(numbers, idx + 1, sum - numbers[idx], target);
+        int withPlus  = dfs(idx + 1, sum + numbers[idx]);
+        int withMinus = dfs(idx + 1, sum - numbers[idx]);
+        
         return withPlus + withMinus;
     }
 }
