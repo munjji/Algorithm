8e68a7b34dac606a2ea907553b527653bb2a1ee5
이지희 <129031670+munjji@users.noreply.github.com>
2026-05-30 15:10:21 +0900

    [level 3] Title: 가장 먼 노드, Time: 19.21 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,25 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        List<Integer>[] graph = new ArrayList[n+1];
-        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+        List<Integer>[] graph = new ArrayList[n + 1];
         
-        for (int[] e: edge) {
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for (int[] e : edge) {
             int u = e[0];
             int v = e[1];
+            
             graph[u].add(v);
             graph[v].add(u);
         }
         
-        int[] dist = new int[n+1];
+        int[] dist = new int[n + 1];
         Arrays.fill(dist, -1);
-        Queue<Integer> q = new ArrayDeque<>();
-        
         dist[1] = 0;
+        
+        Queue<Integer> q = new LinkedList<>();
         q.offer(1);
         
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int cur = q.poll();
+            
             for (int next : graph[cur]) {
                 if (dist[next] == -1) {
                     dist[next] = dist[cur] + 1;
@@ -29,14 +34,11 @@ class Solution {
             }
         }
         
-        int max = 0;
-        for (int i = 1; i <= n; i++) {
-            max = Math.max(max, dist[i]);
-        }
+        int max = Arrays.stream(dist).max().orElse(-1);
         
         int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            if (max == dist[i]) answer++;
+        for (int d : dist) {
+            if (d == max) answer++;
         }
         
         return answer;
